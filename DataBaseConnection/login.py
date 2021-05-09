7a5553b513022a522c50b6048141f31653df1f7e
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2021-05-08 23:30:51 -0300

    Login Executable Update and Makefile Creation

--- DataBaseConnection/login.py
+++ DataBaseConnection/login.py
@@ -1,5 +1,7 @@
 import sys
+import os
 import requests
+
 
 login_url = "http://127.0.0.1:8000/login/"
 data_url = "http://127.0.0.1:8000/data/"
@@ -19,7 +21,11 @@ else:
 data = dict(username=str(sys.argv[1]), password=sys.argv[2], csrfmiddlewaretoken=csrftoken, next='/')
 r = client.post(login_url, data=data, headers=dict(Referer=login_url))
 
-file = open("score.txt", "w")
+path = os.getcwd()
+os.chdir('Assets/Data/')
+path = os.getcwd()
+
+file = open("score.txt", "w")  
 if r.status_code == 200:
     r = client.get(data_url)
     string = '{\"hits\": ' + str(r.json()['hits']) + ', \"mistakes\": ' + str(r.json()['mistakes']) + '}'
---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	python website/manage.py runserver
